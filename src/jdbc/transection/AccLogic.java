04fef90ee0c5d8fd8ac12d317d365262dd7d13ca
HANSOL PARK <ruflthfl00@naver.com>
2022-10-15 15:40:40 +0900

    2022. 10. 15.

--- src/jdbc/transection/AccLogic.java
+++ src/jdbc/transection/AccLogic.java
@@ -44,21 +44,21 @@ public class AccLogic
 			con.setAutoCommit(false);
 			
 		//	 3. 출금계좌에서 이체금액을 빼기
-		String sqlSend = "UPDATE ACCOUNT SET amount -= ? WHERE account_num = ? ";
+		String sqlSend = "UPDATE ACCOUNT SET amount -= ? WHERE account_num = ? "; // 출금 금액과 계좌번호 
 			PreparedStatement psSend = con.prepareStatement(sqlSend);
-				psSend.setInt(1, amount);
-				psSend.setString(2, sendAcc);
+				psSend.setInt(1, amount); 		// 송금 금액
+				psSend.setString(2, sendAcc);  // 송금 계좌
 				
 				int reSend = psSend.executeUpdate();
 				if(reSend == 0) { // 보내는 계좌와 금액이 0일 경우
 					return -1; 		  // 사용자에게 잘못됐다는 신호를 주기 위해서 -1 리턴
-				}
+				} // end of if
 			
 		//	 4. 입금계좌에 이체금액을 더한다
-		String sqlRecv = "UPDATE ACCOUNT SET amount += ? WHERE account_num = ? ";
+		String sqlRecv = "UPDATE ACCOUNT SET amount += ? WHERE account_num = ? "; // 입금 금액과 계좌번호
 	 		PreparedStatement psRec = con.prepareStatement(sqlRecv);
-	 			psRec.setInt(1, amount);
-	 			psRec.setString(2, recvAcc);
+	 			psRec.setInt(1, amount);			// 입금 금액
+	 			psRec.setString(2, recvAcc);		// 입금 계좌
 	 			int reRecv = psRec.executeUpdate();
 	 			if (reRecv == 0) {
 	 				con.rollback();
@@ -76,10 +76,10 @@ public class AccLogic
 			try {	con.close(); } catch (Exception ex) { }
 	
 		}
-		//	 - 만일 정상적인 경우는 0을 리턴하고 도중에 잘못되었으면 트랜잭션을 롤백시키고 -1을 리턴
+		//	 만일 정상적인 경우는 0을 리턴하고 도중에 잘못되었으면 트랜잭션을 롤백시키고 -1을 리턴
 
 		return 0;
-	}
+	} // end of moveAccount()
 
 	//=======================================================
 	//	보내는계좌번호와 받는계좌번호를 넘겨받아
